47eff00aa9d14e0e1d1635156d6ae28c86daf5a9
Spyros Argalias <sargalias@gmail.com>
2019-06-21 18:21:32 +0100

    feat(DrumPad): Add DrumPad component

---
+++ src/components/DrumPad/DrumPad.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './DrumPad.scss';
+
+const DrumPad = () => (
+  <button className={styles.DrumPad} type="button">
+    W
+  </button>
+);
+
+export default DrumPad;
