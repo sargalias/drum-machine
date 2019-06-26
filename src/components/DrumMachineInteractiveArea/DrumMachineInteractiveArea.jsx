3a04600753bd6f55925a703ae395ae96002aa3b7
Spyros Argalias <sargalias@gmail.com>
2019-06-26 22:59:07 +0100

    feat(DrumMachineInteractiveArea): Add component

---
+++ src/components/DrumMachineInteractiveArea/DrumMachineInteractiveArea.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DrumPadCollection from 'components/DrumPadCollection';
+import SoundDisplay from 'components/SoundDisplay';
+
+const DrumMachineInteractiveArea = () => (
+  <div>
+    <DrumPadCollection />
+    <SoundDisplay />
+  </div>
+);
+
+export default DrumMachineInteractiveArea;
