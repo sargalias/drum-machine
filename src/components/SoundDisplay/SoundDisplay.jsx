deb1cb694b748a471e7b154cff83d11819aeb20c
Spyros Argalias <sargalias@gmail.com>
2019-06-26 22:49:16 +0100

    feat(SoundDisplay): Add SoundDisplay component

---
+++ src/components/SoundDisplay/SoundDisplay.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './SoundDisplay.scss';
+
+/* eslint-disable jsx-a11y/label-has-for, jsx-a11y/label-has-associated-control */
+const SoundDisplay = () => (
+  <div className={styles.SoundDisplay}>
+    <label
+      htmlFor="soundName"
+      className={styles.SoundDisplay_label}
+      id="soundNameLabel"
+    >
+      Sound name
+      <output id="soundName" className={styles.SoundDisplay_output}>
+        --------
+      </output>
+    </label>
+  </div>
+);
+/* eslint-enable jsx-a11y/label-has-for, jsx-a11y/label-has-associated-control */
+
+export default SoundDisplay;
