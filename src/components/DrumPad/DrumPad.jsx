9f76f3829a15a45638c5bd995c83df70cb5ad027
Spyros Argalias <sargalias@gmail.com>
2019-07-16 02:49:57 +0100

    feat(DrumPad): Render audio element with src based on prop

--- src/components/DrumPad/DrumPad.jsx
+++ src/components/DrumPad/DrumPad.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 import styles from './DrumPad.scss';
 
-const DrumPad = ({ letter }) => (
+const DrumPad = ({ letter, audioSrc }) => (
   <button className={styles.DrumPad} type="button">
     {letter}
+    <audio src={audioSrc} data-testid={`drumPad_audio_${letter}`} />
   </button>
 );
 
--- src/components/DrumPad/DrumPad.test.jsx
+++ src/components/DrumPad/DrumPad.test.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from 'testUtils';
+import { render, cleanup } from 'testUtils';
 import DrumPad from './DrumPad';
 
 describe('DrumPad', () => {
+  beforeEach(cleanup);
+
   test('should display correct letter', () => {
     const letter = 'W';
 
@@ -10,5 +12,16 @@ describe('DrumPad', () => {
     const btn = getByText(letter);
 
     expect(btn.textContent).toBe(letter);
+  });
+
+  test('should render audio element with audio src passed as prop', () => {
+    const audioSrc = 'https://test.com/';
+    const letter = 'W';
+    const { getByTestId } = render(
+      <DrumPad letter={letter} audioSrc={audioSrc} />,
+    );
+    const audioElement = getByTestId(`drumPad_audio_${letter}`);
+
+    expect(audioElement.src).toBe(audioSrc);
   });
 });
