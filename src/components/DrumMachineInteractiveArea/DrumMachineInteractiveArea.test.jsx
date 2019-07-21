1d7df2495fe49e4da9c6aea0c6f1aa712bed4294
Spyros Argalias <sargalias@gmail.com>
2019-07-22 00:15:18 +0100

    refactorTest(DrumMachineInteractiveArea): Group render tests together

--- src/components/DrumMachineInteractiveArea/DrumMachineInteractiveArea.test.jsx
+++ src/components/DrumMachineInteractiveArea/DrumMachineInteractiveArea.test.jsx
@@ -6,39 +6,41 @@ import drumPads from './drumPadsHelper';
 describe('DrumMachineInteractiveArea', () => {
   beforeEach(cleanup);
 
-  test('renders correct drumPads with empty drumPads prop', () => {
-    const drumPadsProp = [];
+  describe('renders', () => {
+    test('correct drumPads with empty drumPads prop', () => {
+      const drumPadsProp = [];
 
-    const { getAllByTestId } = render(
-      <DrumMachineInteractiveArea drumPads={drumPadsProp} />,
-    );
+      const { getAllByTestId } = render(
+        <DrumMachineInteractiveArea drumPads={drumPadsProp} />,
+      );
 
-    expect(() => getAllByTestId(/drumPad_button_*/)).toThrow();
-  });
-
-  test('renders correct drumPads with normal drumPads prop', () => {
-    const { getByTestId } = render(
-      <DrumMachineInteractiveArea drumPads={drumPads} />,
-    );
-
-    drumPads.forEach(({ letter, audioSrc }) => {
-      const button = getByTestId(`drumPad_button_${letter}`);
-      const audio = getByTestId(`drumPad_audio_${letter}`);
-      expect(button.textContent).toBe(letter);
-      expect(audio.getAttribute('src')).toBe(audioSrc);
+      expect(() => getAllByTestId(/drumPad_button_*/)).toThrow();
     });
-  });
 
-  test('renders soundDisplay component', () => {
-    const { getByTestId: getByTestId1 } = render(
-      <DrumMachineInteractiveArea drumPads={[]} />,
-    );
-    expect(() => getByTestId1('soundDisplay')).not.toThrow();
-    cleanup();
+    test('correct drumPads with normal drumPads prop', () => {
+      const { getByTestId } = render(
+        <DrumMachineInteractiveArea drumPads={drumPads} />,
+      );
 
-    const { getByTestId: getAllByTestId2 } = render(
-      <DrumMachineInteractiveArea drumPads={drumPads} />,
-    );
-    expect(() => getAllByTestId2('soundDisplay')).not.toThrow();
+      drumPads.forEach(({ letter, audioSrc }) => {
+        const button = getByTestId(`drumPad_button_${letter}`);
+        const audio = getByTestId(`drumPad_audio_${letter}`);
+        expect(button.textContent).toBe(letter);
+        expect(audio.getAttribute('src')).toBe(audioSrc);
+      });
+    });
+
+    test('soundDisplay component', () => {
+      const { getByTestId: getByTestId1 } = render(
+        <DrumMachineInteractiveArea drumPads={[]} />,
+      );
+      expect(() => getByTestId1('soundDisplay')).not.toThrow();
+      cleanup();
+
+      const { getByTestId: getAllByTestId2 } = render(
+        <DrumMachineInteractiveArea drumPads={drumPads} />,
+      );
+      expect(() => getAllByTestId2('soundDisplay')).not.toThrow();
+    });
   });
 });
