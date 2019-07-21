0b15111e971a24001f707cec141ee4aecbdfb617
Spyros Argalias <sargalias@gmail.com>
2019-07-22 00:09:29 +0100

    wip(E2E tests): Add test for play audio on drumPad click

--- cypress/e2e/App/App.js
+++ cypress/e2e/App/App.js
@@ -1,3 +1,19 @@
-it('visit', () => {
+/* eslint-disable */
+
+// WIP
+it.skip('drumPad should play audio file', () => {
+  let audioElement;
+
   cy.visit('/');
+
+  cy.getByTestId('drumPad_audio_Q').then($el => {
+    audioElement = $el[0];
+    cy.spy(audioElement, 'play');
+  });
+
+  cy.getByTestId('drumPad_button_Q')
+    .click()
+    .then(() => {
+      expect(audioElement.play).called;
+    });
 });
