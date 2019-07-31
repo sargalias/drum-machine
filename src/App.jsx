de470fc6941337f269f8030d49c15f2a4c29c5f9
Spyros Argalias <sargalias@gmail.com>
2019-07-31 04:27:51 +0100

    feat(App): Add soundNames to drumPads data

--- src/App.jsx
+++ src/App.jsx
@@ -18,34 +18,42 @@ const drumPads = [
   {
     letter: 'Q',
     audioSrc: ambiArpD120_2,
+    soundName: 'Ambience Arp D120 2',
   },
   {
     letter: 'W',
     audioSrc: ambiArpD120_5,
+    soundName: 'Ambience Arp D120 5',
   },
   {
     letter: 'E',
     audioSrc: convLoop120E_1,
+    soundName: 'Convoluted loop 120E 1',
   },
   {
     letter: 'R',
     audioSrc: convLoop120F_4,
+    soundName: 'Convoluted loop 120F 4',
   },
   {
     letter: 'A',
     audioSrc: convLoop120F_5,
+    soundName: 'Convoluted loop 120F 5',
   },
   {
     letter: 'S',
     audioSrc: convLoop120G_4,
+    soundName: 'Convoluted loop 120G 4',
   },
   {
     letter: 'D',
     audioSrc: guitVerber120A_1,
+    soundName: 'Guitar Verberation 120A 1',
   },
   {
     letter: 'F',
     audioSrc: guitVerber120A_2,
+    soundName: 'Guitar Verberation 120A 2',
   },
 ];
 
